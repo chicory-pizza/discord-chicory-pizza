7d0b08dc6e7beaf98d7c7bc2fe461db791fecd48
Ng Yik Phang <ngyikp@gmail.com>
2026-04-20 21:21:57 +0100

    Update to ESLint v10
    Temporarily removing eslint-plugin-jsx-a11y as it's not
    compatible yet

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,36 +1,48 @@
 import js from '@eslint/js';
 import eslintReact from '@eslint-react/eslint-plugin';
 import vitest from '@vitest/eslint-plugin';
-import importPlugin from 'eslint-plugin-import';
-import jsxA11y from 'eslint-plugin-jsx-a11y';
+import {defineConfig} from 'eslint/config';
+import {importX} from 'eslint-plugin-import-x';
+//import jsxA11y from 'eslint-plugin-jsx-a11y';
 import reactHooks from 'eslint-plugin-react-hooks';
 import testingLibrary from 'eslint-plugin-testing-library';
 import tseslint from 'typescript-eslint';
 
-export default tseslint.config(
+export default defineConfig(
 	js.configs.recommended,
 	tseslint.configs.strictTypeChecked,
 
-	importPlugin.flatConfigs.typescript,
-	jsxA11y.flatConfigs.recommended,
-	eslintReact.configs['recommended-type-checked'],
-	reactHooks.configs['recommended-latest'],
+	importX.flatConfigs.typescript,
+	// Temporarily removed to unblock ESLint v10
+	// jsxA11y.flatConfigs.recommended,
+	eslintReact.configs['strict-type-checked'],
+	reactHooks.configs.flat.recommended,
 	{
 		rules: {
 			'no-var': 'error',
 			'prefer-const': 'warn',
 
-			'@eslint-react/no-leaked-conditional-rendering': 'error',
-			'@eslint-react/prefer-read-only-props': 'error',
+			'@eslint-react/immutability': 'error',
+			// ref as prop (https://react.dev/blog/2024/12/05/react-19#ref-as-a-prop)
+			// is not supported on Preact yet
+			'@eslint-react/no-forward-ref': 'off',
+			'@eslint-react/prefer-destructuring-assignment': 'off',
 
-			// `importPlugin.flatConfigs.recommended` without slow rules
+			'@typescript-eslint/no-empty-object-type': [
+				'error',
+				{
+					allowObjectTypes: 'always',
+				},
+			],
+
+			// `importX.flatConfigs.recommended` without slow rules
 			// https://typescript-eslint.io/troubleshooting/typed-linting/performance/#eslint-plugin-import
-			'import/export': 'error',
-			'import/no-duplicates': 'warn',
-			'import/no-named-as-default': 'warn',
+			'import-x/export': 'error',
+			'import-x/no-named-as-default': 'warn',
+			'import-x/no-duplicates': 'warn',
 
-			'import/enforce-node-protocol-usage': ['error', 'always'],
-			'import/order': [
+			// 'import/enforce-node-protocol-usage': ['error', 'always'],
+			'import-x/order': [
 				'warn',
 				{
 					'newlines-between': 'always',
